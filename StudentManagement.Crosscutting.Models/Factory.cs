c15cb94db53407c81544477d635d9a36452a8c48
Rafael Gayoso <gayoso0597@gmail.com>
2022-04-01 23:29:45 +0200

    feat: implemented factory method pattern closes #4

---
+++ StudentManagement.Crosscutting.Models/Factory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Crosscutting.Models
+{
+    public enum Factory
+    {
+        JSON = 1,
+        XML,
+        TXT
+    }
+}

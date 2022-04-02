295a5e44fc27a91ded33ed54a99f5fd9bba5057b
Rafael Gayoso <gayoso0597@gmail.com>
2022-04-02 22:50:07 +0200

    feat : implemented XmlRepository GetAllStudents closes #17

--- StudentManagement.Presentation.WinSite/frmStudent.Designer.cs
+++ StudentManagement.Presentation.WinSite/frmStudent.Designer.cs
@@ -33,27 +33,30 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.addBtn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.comboFile);
-            this.groupBox1.Location = new System.Drawing.Point(36, 493);
+            this.groupBox1.Location = new System.Drawing.Point(32, 493);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(409, 90);
+            this.groupBox1.Size = new System.Drawing.Size(303, 73);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Origen de Datos";
+            this.groupBox1.Text = "Data Origin";
             // 
             // comboFile
             // 
             this.comboFile.Cursor = System.Windows.Forms.Cursors.Hand;
             this.comboFile.FormattingEnabled = true;
-            this.comboFile.Location = new System.Drawing.Point(16, 31);
+            this.comboFile.Location = new System.Drawing.Point(18, 25);
             this.comboFile.Name = "comboFile";
-            this.comboFile.Size = new System.Drawing.Size(352, 28);
+            this.comboFile.Size = new System.Drawing.Size(269, 28);
             this.comboFile.TabIndex = 0;
             this.comboFile.SelectedIndexChanged += new System.EventHandler(this.indexChange);
             // 
@@ -64,12 +67,12 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(743, 419);
+            this.dataGridView1.Size = new System.Drawing.Size(697, 419);
             this.dataGridView1.TabIndex = 1;
             // 
             // addBtn
             // 
-            this.addBtn.Location = new System.Drawing.Point(576, 528);
+            this.addBtn.Location = new System.Drawing.Point(6, 25);
             this.addBtn.Name = "addBtn";
             this.addBtn.Size = new System.Drawing.Size(99, 39);
             this.addBtn.TabIndex = 2;
@@ -79,28 +82,50 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(488, 562);
+            this.button1.Location = new System.Drawing.Point(275, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 30);
+            this.button1.Size = new System.Drawing.Size(98, 39);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.addBtn);
+            this.groupBox2.Location = new System.Drawing.Point(350, 493);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(379, 73);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operations";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // frmStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(788, 629);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.addBtn);
+            this.ClientSize = new System.Drawing.Size(764, 573);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.Name = "frmStudent";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmStudent_Load);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -112,6 +137,8 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button addBtn;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
